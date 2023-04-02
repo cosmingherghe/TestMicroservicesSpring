4edf2e4ea76579e07a1583d97b3bfd7c56ba116a
cosmingherghe <cosminlinux@gmail.com>
2023-04-02 17:30:18 -0400

    @Builder EmployeeResponse

--- employee-service/src/main/java/dev/cosmingherghe/employeeapp/controller/EmployeeController.java
+++ employee-service/src/main/java/dev/cosmingherghe/employeeapp/controller/EmployeeController.java
@@ -2,9 +2,11 @@ package dev.cosmingherghe.employeeapp.controller;
 
 import dev.cosmingherghe.employeeapp.entity.Employee;
 import dev.cosmingherghe.employeeapp.repository.EmployeeRepository;
+import dev.cosmingherghe.employeeapp.response.EmployeeResponse;
+import dev.cosmingherghe.employeeapp.service.EmployeeService;
+import lombok.Builder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,11 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
     @Autowired
-    EmployeeRepository employeeRepo;
+    EmployeeService employeeServ;
 
     @GetMapping("/employee/{id}")
-    public ResponseEntity<Employee> getEmployeeDetails(@PathVariable("id") String id) {
-        Employee employee = employeeRepo.findById(Long.valueOf(id)).get();
-        return ResponseEntity.status(HttpStatus.OK).body(employee);
+    public ResponseEntity<EmployeeResponse> getEmployeeDetails(@PathVariable("id") String id) {
+        EmployeeResponse employeeResponse = employeeServ.getEmployeeResponseById(Long.valueOf(id));
+        return ResponseEntity.status(HttpStatus.OK).body(employeeResponse);
     }
 }

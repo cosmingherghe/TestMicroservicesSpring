96328a190e6e54c7febc134daa3b2dee1e73dece
cosmingherghe <cosminlinux@gmail.com>
2023-04-02 17:15:10 -0400

    ResponseEntity with HttpStatus.OK

--- employee-service/src/main/java/dev/cosmingherghe/employeeapp/controller/EmployeeController.java
+++ employee-service/src/main/java/dev/cosmingherghe/employeeapp/controller/EmployeeController.java
@@ -3,7 +3,9 @@ package dev.cosmingherghe.employeeapp.controller;
 import dev.cosmingherghe.employeeapp.entity.Employee;
 import dev.cosmingherghe.employeeapp.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +16,8 @@ public class EmployeeController {
     EmployeeRepository employeeRepo;
 
     @GetMapping("/employee/{id}")
-    public String getEmployeeDetails(@PathVariable("id") String id) {
-        return  employeeRepo.findById(Long.valueOf(id)).isPresent() ?
-                employeeRepo.findById(Long.valueOf(id)).get().toString() :
-                "Employee Not Found ID: " + id;
+    public ResponseEntity<Employee> getEmployeeDetails(@PathVariable("id") String id) {
+        Employee employee = employeeRepo.findById(Long.valueOf(id)).get();
+        return ResponseEntity.status(HttpStatus.OK).body(employee);
     }
 }

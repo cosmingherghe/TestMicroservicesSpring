1d39001260bca0920090ffebbaaaff424ddddecd
cosmingherghe <cosminlinux@gmail.com>
2023-04-04 01:14:58 -0400

    Address Repo

--- address-service/src/main/java/dev/cosmingherghe/addressapp/repository/AddressRepository.java
+++ address-service/src/main/java/dev/cosmingherghe/addressapp/repository/AddressRepository.java
@@ -3,6 +3,11 @@ package dev.cosmingherghe.addressapp.repository;
 
 import dev.cosmingherghe.addressapp.entity.Address;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface AddressRepository extends ListCrudRepository<Address, Long> {
+    Address findByEmployeeId(Long id);
 }

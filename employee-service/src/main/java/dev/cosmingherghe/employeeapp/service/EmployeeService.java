7720eb4beb58e271fabb9227a0571661713a9644
cosmingherghe <cosminlinux@gmail.com>
2023-04-04 02:28:11 -0400

    RestTemplate and getAddressResponse

--- employee-service/src/main/java/dev/cosmingherghe/employeeapp/service/EmployeeService.java
+++ employee-service/src/main/java/dev/cosmingherghe/employeeapp/service/EmployeeService.java
@@ -2,22 +2,34 @@ package dev.cosmingherghe.employeeapp.service;
 
 import dev.cosmingherghe.employeeapp.entity.Employee;
 import dev.cosmingherghe.employeeapp.repository.EmployeeRepository;
+import dev.cosmingherghe.employeeapp.response.AddressResponse;
 import dev.cosmingherghe.employeeapp.response.EmployeeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmployeeService {
     @Autowired
     EmployeeRepository employeeRepo;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     public EmployeeResponse getEmployeeResponseById(Long id) {
         Employee employee = employeeRepo.findById(id).get();
-        EmployeeResponse employeeResponse = EmployeeResponse.builder()
+        String url = "http://localhost:8082/address-app/api/v1/address/";
+        AddressResponse addressResponse = getAddressResponse(employee, url);
+        return EmployeeResponse.builder()
                 .id(employee.getId())
                 .name(employee.getName())
                 .bloodgroup(employee.getBloodgroup())
+                .addressResponse(addressResponse)
                 .build();
-        return employeeResponse;
+    }
+
+    private AddressResponse getAddressResponse(Employee employee, String url) {
+        AddressResponse addressResponse = restTemplate.getForObject(url +"{id}", AddressResponse.class, employee.getAddressId());
+        return addressResponse;
     }
 }

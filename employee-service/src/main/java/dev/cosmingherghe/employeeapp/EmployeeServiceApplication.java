ddcb34a9b4769b56dcf485dbf55fe367100328a7
cosmingherghe <cosminlinux@gmail.com>
2023-04-04 02:26:36 -0400

    RestTemplate bean

--- employee-service/src/main/java/dev/cosmingherghe/employeeapp/EmployeeServiceApplication.java
+++ employee-service/src/main/java/dev/cosmingherghe/employeeapp/EmployeeServiceApplication.java
@@ -2,6 +2,8 @@ package dev.cosmingherghe.employeeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
@@ -9,5 +11,8 @@ public class EmployeeServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
